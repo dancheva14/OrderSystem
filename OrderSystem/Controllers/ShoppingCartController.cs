56358af47eeed265b5e6bf990a820bd0f961847a
Vivian Dancheva <dancheva14@abv.bg>
2020-09-11 16:44:55 +0300

    Added cart quantity

--- OrderSystem/Controllers/ShoppingCartController.cs
+++ OrderSystem/Controllers/ShoppingCartController.cs
@@ -49,6 +49,7 @@ namespace OrderSystem.Controllers
                         order.OrderDetails.Add(new OrderDetail { Part = part, Price = part.Pricee, Quantity = 1 });
                     }
                     order.Number = random.Next(1000, 100000).ToString();
+
                     return View(order);
                 }
                 else
@@ -61,30 +62,25 @@ namespace OrderSystem.Controllers
         [HttpPost]
         public IActionResult Cart(Order order)
         {
-
-            var ids = HttpContext.Session.GetString("itemsToCart");
-            if (!string.IsNullOrEmpty(ids))
+            var newOrder = new Order();
+            newOrder.OrderDetails = new List<OrderDetail>();
+            Random random = new Random();
+            foreach (var orderDetail in order.OrderDetails)
             {
-                Random random = new Random();
-                var parts = partsService.GetPartsByIds(ids.ToString());
-                order.OrderDetails = new List<OrderDetail>();
-                foreach (var part in parts)
-                {
-                    order.OrderDetails.Add(new OrderDetail { PartId = part.PartId, Price = part.Pricee, Quantity = 1, Part = null }) ;
-                }
-                order.Number = random.Next(1000, 100000).ToString();
+                newOrder.OrderDetails.Add(new OrderDetail { PartId = orderDetail.Part.PartId, Price = orderDetail.Part.Pricee, Quantity = orderDetail.Quantity, Part = null });
             }
-            order.Date = DateTime.Now;
-            order.Amount = order.OrderDetails.Sum(a => a.Price * a.Quantity);
-            order.PartnerId = order.PartnerId;
-            order.Address = order.Address;
-            order.StatusId = statusService.GetStatuss().FirstOrDefault().StatusId;
-            order.UserId = userService.GetUsers().FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
-            order.Partner = null;
-            order.Status = null;
-            order.User = null;
+            newOrder.Number = random.Next(1000, 100000).ToString();
+            newOrder.Date = DateTime.Now;
+            newOrder.Amount = order.OrderDetails.Sum(a => a.Price * a.Quantity);
+            newOrder.PartnerId = order.PartnerId;
+            newOrder.Address = order.Address;
+            newOrder.StatusId = statusService.GetStatuss().FirstOrDefault().StatusId;
+            newOrder.UserId = userService.GetUsers().FirstOrDefault(u => u.UserName == User.Identity.Name).Id;
+            newOrder.Partner = null;
+            newOrder.Status = null;
+            newOrder.User = null;
 
-            ordersService.AddOrder(order);
+            ordersService.AddOrder(newOrder);
 
             HttpContext.Session.SetString("itemsToCart", string.Empty);
 
@@ -96,7 +92,7 @@ namespace OrderSystem.Controllers
         {
             var ids = HttpContext.Session.GetString("itemsToCart");
 
-            if (!string.IsNullOrEmpty(ids))
+            if (id != 0)
             {
                 List<string> idArr = ids.Split(",").ToList();
 
@@ -105,7 +101,7 @@ namespace OrderSystem.Controllers
                 foreach (var item in idArr)
                 {
                     if (!string.IsNullOrEmpty(newIds))
-                        newIds += item;
+                        newIds += "," + item;
                     else
                         newIds = item;
                 }
